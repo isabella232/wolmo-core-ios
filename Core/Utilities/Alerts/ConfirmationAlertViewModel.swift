ee7dea4fad8fee686dd2f0c3d985805cdb08d5f3
Francisco Depascuali <francisco.depascuali@wolox.com.ar>
2016-07-06 14:53:17 -0300

    Alerts: default titles for dismiss and confirm buttons

--- Core/Utilities/Alerts/ConfirmationAlertViewModel.swift
+++ Core/Utilities/Alerts/ConfirmationAlertViewModel.swift
@@ -8,11 +8,13 @@
 
 import Foundation
 
-/// ConfirmationAlertViewModel models a `UIAlertController` where there are
-/// two options to select:
-///
-/// * confirmAction: normally a action that do something.
-/// * dismissAction: normally a empty action.
+/**
+ ConfirmationAlertViewModel models a `UIAlertController` where there are
+ two options to select:
+
+ * confirmAction: normally a action that do something.
+ * dismissAction: normally a empty action.
+*/
 public struct ConfirmationAlertViewModel {
     
     public let title: String
@@ -22,23 +24,25 @@ public struct ConfirmationAlertViewModel {
     public let dismissAction: ConfirmationAlertViewModel -> ()
     public let confirmAction: ConfirmationAlertViewModel -> ()
     
-    /// Initialize a new ConfirmationAlertViewModel with the provided parameters.
-    ///
-    /// Is not necessary to provide all parameters. In most cases, is only
-    /// needed `title`, `message` and `confirmAtion`.
-    ///
-    /// - parameter title: The alert title.
-    /// - parameter message: The alert message.
-    /// - parameter dismissButtonTitle: The dismiss button title.
-    /// - parameter dismissAction: The dismiss button action.
-    /// - parameter confirmButtonTitle: The confirm button title.
-    /// - parameter confirmAction: The confirm button action.
+    /**
+    Initialize a new ConfirmationAlertViewModel with the provided parameters.
+    
+    Is not necessary to provide all parameters. In most cases, is only
+    needed `title`, `message` and `confirmAtion`.
+    
+    - parameter title: The alert title.
+    - parameter message: The alert message.
+    - parameter dismissButtonTitle: The dismiss button title.
+    - parameter dismissAction: The dismiss button action.
+    - parameter confirmButtonTitle: The confirm button title.
+    - parameter confirmAction: The confirm button action.
+    */
     public init(
         title: String,
         message: String,
-        dismissButtonTitle: String = "confirmation-alert-view.dismiss.title".localized,
+        dismissButtonTitle: String = DefaultConfirmButtonTitle,
         dismissAction: ConfirmationAlertViewModel -> () = { _ in },
-        confirmButtonTitle: String = "confirmation-alert-view.confirm.title".localized,
+        confirmButtonTitle: String = DefaultDismissButtonTitle,
         confirmAction: ConfirmationAlertViewModel -> () = { _ in }) {
         self.title = title
         self.message = message
@@ -48,4 +52,7 @@ public struct ConfirmationAlertViewModel {
         self.confirmButtonTitle = confirmButtonTitle
     }
     
+    public static var DefaultConfirmButtonTitle: String = "confirmation-alert-view.dismiss.title".localized
+    
+    public static var DefaultDismissButtonTitle: String = "confirmation-alert-view.confirm.title".localized
 }
