0e558af329bb288d151ce3e0cbe629d4582caa62
Francisco Depascuali <francisco.depascuali@wolox.com.ar>
2016-07-15 12:25:09 -0300

    Correcting signal's flatMap comment

--- Core/Extensions/ReactiveCocoa/Signal.swift
+++ Core/Extensions/ReactiveCocoa/Signal.swift
@@ -13,7 +13,7 @@ public extension Signal {
     /**
      Transforms a `Signal<Value, Error>` to `Signal<Value, NewError>`
      This is usually useful when the `flatMap` operator is used and the outer
-     producer as `NoError` and the inner one a different type of error.
+     signal has `NoError` error type and the inner one a different type of error.
      
      - returns: A signal with the same value type but with `NewError` as the error type
      */
