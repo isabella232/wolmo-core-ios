a9f3b7f3ce56b6b3a4b0952ff6d5c40c2ba264b4
Francisco Depascuali <francisco.depascuali@wolox.com.ar>
2016-07-19 11:08:11 -0300

    Adding borders and comments

--- Core/Extensions/UIKit/UIView/UIView.swift
+++ Core/Extensions/UIKit/UIView/UIView.swift
@@ -26,6 +26,68 @@ public enum ViewPositioning {
 extension UIView {
     
     /**
+     Adds a border to the top of the view.
+     
+     - parameter border: Models the border to be added
+     - parameter offset: The offset for the width of the border related to self.
+     The width of the border will be view.width - offset.
+     
+     Note: By default, the border will have the width of the view.
+     */
+    public func topBorder(border: Border, offset: CGFloat = 0) {
+        let borderView = UIView(frame: CGRect(x: 0, y: 0, width: bounds.width - offset, height: CGFloat(border.height)))
+        borderView.backgroundColor = border.color
+        addSubview(borderView)
+    }
+    
+    /**
+     Adds a border to the bottom of the view.
+     
+     - parameter border: Models the border to be added
+     - parameter offset: The offset for the width of the border related to self.
+     The width of the border will be view.width - offset.
+     
+     Note: By default, the border will have the width of the view.
+     */
+    public func bottomBorder(border: Border, offset: CGFloat = 0) {
+        let borderView = UIView(frame: CGRect(x: 0, y: bounds.height - CGFloat(border.height), width: bounds.width - offset,
+            height: CGFloat(border.height)))
+        borderView.backgroundColor = border.color
+        addSubview(borderView)
+    }
+    
+    /**
+     Adds a border to the left of the view.
+     
+     - parameter border: Models the border to be added
+     - parameter offset: The offset for the height of the border related to self.
+     The height of the border will be view.height - offset.
+     
+     Note: By default, the border will have the height of the view.
+     */
+    public func leftBorder(border: Border, offset: CGFloat = 0) {
+        let borderView = UIView(frame: CGRect(x: 0, y: 0, width: CGFloat(border.height), height: bounds.height - offset))
+        borderView.backgroundColor = border.color
+        addSubview(borderView)
+    }
+    
+    /**
+     Adds a border to the right of the view.
+     
+     - parameter border: Models the border to be added
+     - parameter offset: The offset for the height of the border related to self.
+     The width of the border will be view.height - offset.
+     
+     Note: By default, the border will have the height of the view.
+     */
+    public func rightBorder(border: Border, offset: CGFloat = 0) {
+        let borderView = UIView(frame: CGRect(x: bounds.width - CGFloat(border.height), y: 0, width: CGFloat(border.height),
+            height: bounds.height - offset))
+        borderView.backgroundColor = border.color
+        addSubview(borderView)
+    }
+    
+    /**
      Loads the view into the specified containerView.
      
      - warning: It must be done after self's view is loaded.
