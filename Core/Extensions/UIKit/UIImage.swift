41d8f15b77ee1744c1db9ba21e6bb4ff9d961655
Nahuel Gladstein <nahuel.gladstein@wolox.com.ar>
2017-06-12 16:23:24 -0300

    Add documentation

--- Core/Extensions/UIKit/UIImage.swift
+++ Core/Extensions/UIKit/UIImage.swift
@@ -10,6 +10,15 @@ import UIKit
 
 public extension UIImage {
     
+    /**
+     Returns a resized copy of the image maintaining it's aspect ratio, with cornerRadius if used.
+     
+     - parameter wantedSize: The wanted size to fit the new image in.
+        Note that the size of the resulting image will be the closest size to the one specified that maintains the aspect ratio of the original image.
+     - parameter cornerRadius: The cornerRadius to be used. Any number lower or equal to zero will not add cornerRadius. Default: 0.0
+     
+     */
+    
     public func resizedTo(_ wantedSize: CGSize, cornerRadius: CGFloat = 0.0) -> UIImage {
         let newSize = size.resizedMaintainingRatio(wantedSize: wantedSize)
         
@@ -31,6 +40,15 @@ public extension UIImage {
 
 fileprivate extension CGSize {
 
+    /**
+     Returns a resized copy of the size maintaining it's aspect ratio.
+     
+     - parameter wantedSize: The wanted new size to fit.
+     Note that the size of the resulting CGSize will be the closest to the one specified that maintains the aspect ratio of the original.
+     
+     */
+    
+    
     fileprivate func resizedMaintainingRatio(wantedSize: CGSize) -> CGSize {
         let widthFactor = width / wantedSize.width
         let heightFactor = height / wantedSize.height
