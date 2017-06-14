aeda3d7a100bfde87b43ecc8a5ea5dbf34302343
Nahuel Gladstein <nahuel.gladstein@wolox.com.ar>
2017-06-14 14:49:33 -0300

    Add a parameter to choose if the screen scale should be use at resizing

--- Core/Extensions/UIKit/UIImage.swift
+++ Core/Extensions/UIKit/UIImage.swift
@@ -15,14 +15,20 @@ public extension UIImage {
      
      - parameter wantedSize: The wanted size to fit the new image in.
         Note that the size of the resulting image will be the closest size to the one specified that maintains the aspect ratio of the original image.
+     - parameter useScreenScale: Determines if the screen scale will be used when creating the image context. Default: true.
+        Set to true if you want to make the size relative to the scale of the screen 
+            (So the resized image looks the same relative size on all devices, you probably want this if you're showing the resized image).
+        Set to false if you want the size to be actual pixels 
+            (This is useful to generate a resized image of a particular size in real pixels to send to API regardless of the device).
      - parameter cornerRadius: The cornerRadius to be used. Any number lower or equal to zero will not add cornerRadius. Default: 0.0
      
      */
     
-    public func resizedTo(_ wantedSize: CGSize, cornerRadius: CGFloat = 0.0) -> UIImage {
+    public func resizedTo(_ wantedSize: CGSize, useScreenScale: Bool = true, cornerRadius: CGFloat = 0.0) -> UIImage {
         let newSize = size.resizedMaintainingRatio(wantedSize: wantedSize)
+        let scale: CGFloat = useScreenScale ? 0.0 : 1.0
         
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        UIGraphicsBeginImageContextWithOptions(newSize, false, scale)
         if cornerRadius > 0 {
             let path = UIBezierPath(
                         roundedRect:CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height),
@@ -47,7 +53,6 @@ fileprivate extension CGSize {
      Note that the size of the resulting CGSize will be the closest to the one specified that maintains the aspect ratio of the original.
      
      */
-    
     
     fileprivate func resizedMaintainingRatio(wantedSize: CGSize) -> CGSize {
         let widthFactor = width / wantedSize.width
