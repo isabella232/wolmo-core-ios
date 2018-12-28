682d625e3c355d54bf732bc81cc87d3b69674009
Gabriel Leandro Mazzei <gmazzei91@gmail.com>
2018-12-28 16:00:06 -0300

    Pan recognizer with closure (#118)
    * Pan recognizer with closure
    
    * Recognizer as parameter
    
    * Fix - Parameters added
    
    * slather file updated
    
    * Fix - slather.yml

--- WolmoCoreDemo/ViewController.swift
+++ WolmoCoreDemo/ViewController.swift
@@ -36,6 +36,9 @@ final internal class ViewController: UIViewController {
         _view.gestureLabel.addPinchGestureRecognizer {
             print("Label pinched!")
         }
+        _view.gestureLabel.addPanGestureRecognizer { recognizer in
+            print("Label panned!")
+        }
         _view.gestureLabel.addSwipeGestureRecognizer(direction: .left) { recognizer in
             print("Label swiped!")
         }
