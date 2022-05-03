1ae7ffcd03eb1d2a9dde486b4ba443295c5df6f7
Juan Martín Gordo <81249698+jgordowolox@users.noreply.github.com>
2022-05-03 18:39:48 -0300

    Added method for rotating images (#161)

--- WolmoCore/Extensions/UIKit/UIImageView.swift
+++ WolmoCore/Extensions/UIKit/UIImageView.swift
@@ -21,7 +21,7 @@ public extension UIImageView {
     }
     
     /**
-     Sets de image of the view to a drawing of the given string,
+     Sets the image of the view to a drawing of the given string,
      it's recomended for using emojis as images, but it can be used with any string.
      - parameter string: String, the text to draw in the image.
      - parameter font: UIFont to apply to the drawn string.
@@ -29,5 +29,13 @@ public extension UIImageView {
     func draw(string: String, font: UIFont) {
         contentMode = .scaleAspectFit
         image = string.toImage(font: font)
+    }
+    
+    /**
+     Rotates the image of the view a by a certain amount in degrees.
+     */
+    func rotate(degrees: CGFloat) {
+        let radians = degrees * CGFloat.pi / 180
+        transform = CGAffineTransform(rotationAngle: radians)
     }
 }
